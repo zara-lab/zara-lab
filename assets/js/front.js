eb12af2520daee6bb3a8f3a214845d32fb72d0ff
Petko Petkov <p.petkov@zaralab.org>
2018-10-18 16:05:32 +0300

    fixed label

--- assets/js/front.js
+++ assets/js/front.js
@@ -251,7 +251,7 @@ function submitForm(){
       $.post('https://zaralab.azurewebsites.net/academy', data, function(response){
              console.log(response); 
              $('#subscribe-modal').modal('hide');
-             swal("Готово!", "Your imaginary data has been send!", "success");  // success/error 
+             swal("Готово!", "Вашата заявка беше записана, ще се свържем с вас скоро!", "success");  // success/error 
       }).fail(function() {
         swal("Възникна грешка :(", "Опитайте отново или се свържете с нас на email: academy@zaralab.org", "error");
       })
